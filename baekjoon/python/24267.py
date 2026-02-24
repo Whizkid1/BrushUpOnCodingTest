95655107b345129f2a0a244e34b7953f01cb4bbe
Whizkid1 <dongwon1000@naver.com>
2026-02-24 15:44:51 +0900

    24266, 24267, 24313 solved!!

--- baekjoon/python/24267.py
+++ baekjoon/python/24267.py
@@ -7,7 +7,7 @@ for i in range(1, n-1):
 print(sum)
 print(3)
 
-# 수학 공식이 있음....
-n = int(input())
-print(n*(n-1)*(n-2)//6)
-print(3)
+# # 수학 공식이 있음...
+# n = int(input())
+# print(n*(n-1)*(n-2)//6)
+# print(3)
